16a258e1d28c3211ec35965bb3966efc26886ab8
vecin2 <vecin2@gmail.com>
2018-08-16 14:23:12 -0400

    refacto select options

--- sql_gen/sql_gen.py
+++ sql_gen/sql_gen.py
@@ -1,4 +1,4 @@
-wimrom jinja2 import Environment, FileSystemLoader, select_autoescape, meta, Template
+from jinja2 import Environment, FileSystemLoader, select_autoescape, meta, Template
 from sql_gen.template_source import TemplateSource
 from sql_gen.prompter import Prompter
 import os,sys
@@ -12,29 +12,37 @@ class TemplateOption():
 class TemplateSelector():
     def select_template(self, env):
         template_list = env.list_templates(".sql")
-        template_option_list = create_options_to_select()
-            
-        for template_option in template_option_list:
-            print(str(template_option.id) + ". " +template_option.name)
+        self.create_options(template_list)
+        self.show_options()
 
-        template_number = raw_input("Please select template to parse: ")
-
-        return load_template_source_by_option(template_number)
+        template_number = self.prompt_to_select_template()    
+        template_name = self.get_option_by_id(template_number).name
+        return self.build_template_source(template_name,env)
     
-    def create_options_to_select(self):
-        template_option_list=[]
+    def create_options(self, template_list):
+        self.template_option_list=[]
         for counter, template in enumerate(template_list):
             template_option =TemplateOption(counter, template)
-            template_option_list.append(template_option)
+            self.template_option_list.append(template_option)
+        return self.template_option_list
 
-    def get_template_by_id(self, template_option_list, template_number):
-        for template_option in template_option_list:
+    def show_options(self):
+        for template_option in self.template_option_list:
+            print(str(template_option.id) + ". " +template_option.name)
+    def prompt_to_select_template(self):
+        template_number = raw_input("Please select template to parse: ")
+        while self.get_option_by_id(template_number) is None:
+            template_number = raw_input("Please select template to parse: ")
+            self.show_options()
+        return template_number
+
+    def get_option_by_id(self, template_number):
+        for template_option in self.template_option_list:
             if template_number == str(template_option.id):
                 return template_option
         return None
 
-    def load_template_source_by_option(option_number, env):
-        template_name = self.get_template_by_id(template_option_list, template_number).name
+    def build_template_source(self, template_name, env):
         source = env.loader.get_source(env,template_name)[0]
         template_source = TemplateSource(env.parse(source))
         template_source.template_name = template_name
@@ -43,6 +51,7 @@ class TemplateSelector():
 def description(value, description):
     return value
 
+##main
 env = Environment(
                 loader=FileSystemLoader('/home/dgarcia/dev/python/em_dev_tools/sql_gen/templates'),
                 autoescape=select_autoescape(['html', 'xml']))
@@ -50,9 +59,9 @@ env.filters['description']=description
 
 template_selector = TemplateSelector()
 template_source= template_selector.select_template(env)
-
 prompter = Prompter(template_source)
 context = prompter.build_context()
 template = env.get_template(template_source.template_name)
+
 print(template.render(context))
 
