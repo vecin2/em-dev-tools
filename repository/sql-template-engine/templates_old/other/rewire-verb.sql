91f0e44bcf195c78a555f576dc7b8114244b20d2
vecin2 <vecin2@gmail.com>
2018-08-16 14:23:44 -0400

    cleaning examples and tests

--- repository/sql-template-engine/templates_old/other/rewire-verb.sql
+++
@@ -1,5 +0,0 @@
-
-update eva_verb 
-set (PROCESS_DESC_REF_ID) = (@PDR.$PROCESS_DESC_REF_ID)
-where ENTITY_DEF_ID = @ED.$ENTITY_DEF_ID and name ='$VERB_NAME';
-
--- test-connect-to-db.py
+++
@@ -1,7 +0,0 @@
-import pypyodbc
-
-connection = pypyodbc.connect('Driver={ODBC Driver 11 for SQL Serve};'
-                                'Server=windows;'
-                                'Database=ootb_15_1_fp2;'
-                                'uid=sa;pwd=admin')
-connection.close()
